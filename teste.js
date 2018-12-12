141cee33dc59eda0166c009cadc1f0029b36aa57
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2018-12-12 12:04:01 -0200

    tetse

--- teste.js
+++ teste.js
@@ -5,9 +5,7 @@ soap.createClientAsync(url).then((soapClient) => {
     var args = {login: 'botz', senha: '102030mmr@', codigoEntidade: 1581, Cpf: '10797116869'};
     console.log("ola", args)
     soapClient.buscarCartoesDisponiveis(args, function(err,result,raw){
-
-        console.log(result)
-        // if(err) { console.log(err); }
-        // else { console.log('Result: \n' + JSON.stringify(result)); }
+        if(err) { console.log(err); }
+        else { console.log('Result: \n' + JSON.stringify(result)); }
     })
 })
