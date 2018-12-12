fe8915c6db42101fab78e70a0908808e745a831a
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2018-12-12 14:23:32 -0200

    tetse

--- teste.js
+++ teste.js
@@ -5,6 +5,7 @@ var args = {"login": 'botz', "senha": '102030mmr@', "codigoEntidade": 1581, "Cpf
 
 
 soap.createClientAsync(url).then((soapClient) => {
+    console.log(o2x(args))
     soapClient.SaqueComplementarWebServiceService.SaqueComplementar.buscarCartoesDisponiveis(o2x(args), function(err,result){
         if (err) {
             console.log(err)
