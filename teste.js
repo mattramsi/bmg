5f3795b4b1e6bd1a4982fc7d993d25ca8bfd9667
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2018-12-12 18:04:52 -0200

    ajuste xml

--- teste.js
+++ teste.js
@@ -1,13 +1,23 @@
 var soap = require('soap');
 var o2x = require('object-to-xml');
 var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
-var args = {"login": "botz", "senha": "102030mmr@", "codigoEntidade": 1581, "Cpf": "10797116869" };
+var args = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com">\n\
+   <soapenv:Header/>\n\
+   <soapenv:Body>\n\
+      <web:buscarCartoesDisponiveis soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">\n\
+         <param xsi:type="web:CartaoDisponivelParameter">\n\
+            <login xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">botz</login>\n\
+            <senha xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">102030mmr@</senha>\n\
+            <codigoEntidade xsi:type="xsd:int">1581</codigoEntidade>\n\
+            <cpf xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">10797116869</cpf>\n\
+         </param>\n\
+      </web:buscarCartoesDisponiveis>\n\
+   </soapenv:Body>\n\
+</soapenv:Envelope>';
 
-
+console.log(args)
 soap.createClientAsync(url).then((soapClient) => {
-    console.log(o2x(args))
-    console.log(args)
-    soapClient.SaqueComplementarWebServiceService.SaqueComplementar.buscarCartoesDisponiveis(o2x(args), function(err,result){
+    soapClient.SaqueComplementarWebServiceService.SaqueComplementar.buscarCartoesDisponiveis(args, function(err,result){
         if (err) {
             console.log(err)
             throw err;
