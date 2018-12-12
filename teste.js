0e352ca77d1ed0cb20c5066fa3c034e784b36cfd
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2018-12-12 12:20:09 -0200

    tetse

--- teste.js
+++ teste.js
@@ -1,10 +1,10 @@
 var soap = require('soap');
 var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
 
-soap.createClientAsync(url).then((soapClient) => {
+soap.createClientAsync(url, args).then((soapClient) => {
     var args = {login: 'botz', senha: '102030mmr@', codigoEntidade: 1581, Cpf: '10797116869'};
-    console.log("ola", args)
-    soapClient.buscarCartoesDisponiveis(args, function(err,result,raw){
+    console.log("ola", soapClient.lastClient)
+    soapClient.buscarCartoesDisponiveis(function(err,result){
         if(err) { console.log(err); }
         else { console.log('Result: \n' + JSON.stringify(result)); }
     })
