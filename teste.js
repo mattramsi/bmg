7c98c7c254878ca1081babb63de82b23b8834c92
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2018-12-12 14:01:43 -0200

    tetse

--- teste.js
+++ teste.js
@@ -1,9 +1,9 @@
 var soap = require('soap');
 var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
-var args = {login: 'botz', senha: '102030mmr@', codigoEntidade: 1581, Cpf: '10797116869'};
+var args = {"tns:login": 'botz', "tns:senha": '102030mmr@', "tns:codigoEntidade": 1581, "tns:Cpf": '10797116869'};
 console.log("ola", args)
 
-soap.createClientAsync(url, args).then((soapClient) => {
+soap.createClientAsync(url).then((soapClient) => {
     
     soapClient.buscarCartoesDisponiveis(function(err,result){
         if(err) { console.log(err); }
