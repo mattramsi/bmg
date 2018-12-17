e901c645937fa9af08c7c6c9453c3ed0953da6e8
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2018-12-17 11:04:43 -0200

    ajustes

--- teste.js
+++ teste.js
@@ -1,33 +1,25 @@
-var soap = require('soap');
-var DOMParser = require('xmldom').DOMParser;
-
 var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
-var text = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com">\n\
-   <soapenv:Header/>\n\
-   <soapenv:Body>\n\
-      <web:buscarCartoesDisponiveis soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">\n\
-         <param xsi:type="web:CartaoDisponivelParameter">\n\
-            <login xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">botz</login>\n\
-            <senha xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">102030mmr@</senha>\n\
-            <codigoEntidade xsi:type="xsd:int">1581</codigoEntidade>\n\
-            <cpf xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">10797116869</cpf>\n\
-         </param>\n\
-      </web:buscarCartoesDisponiveis>\n\
-   </soapenv:Body>\n\
-</soapenv:Envelope>';
+var xml = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com"><soapenv:Header/><soapenv:Body><web:buscarCartoesDisponiveis soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'+
+         '<param xsi:type="web:CartaoDisponivelParameter">'+
+            '<login xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">botz</login>'+
+            '<senha xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">102030mmr@</senha>'+
+            '<codigoEntidade xsi:type="xsd:int">1581</codigoEntidade>'+
+            '<cpf xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">10797116869</cpf>'+
+         '</param>'+
+      '</web:buscarCartoesDisponiveis>'+
+   '</soapenv:Body>'+
+'</soapenv:Envelope>';
 
+const request = require('request')
 
-var args = DOMParser().parseFromString(text,"text/xml");
+const opts = {
+    body: xml,
+    headers: {
+        'Content-Type': 'text/xml; charset=utf-8',
+        SOAPAction: 'runTransaction'
+    }
+}
 
-console.log(args)
-
-soap.createClientAsync(url).then((soapClient) => {
-    soapClient.SaqueComplementarWebServiceService.SaqueComplementar.buscarCartoesDisponiveis(args, function(err,result){
-        if (err) {
-            console.log(err)
-            throw err;
-        } 
-        
-        console.log(result);
-    })
+const body = request.post(url, opts, (err, response) => {
+    console.log('response', response.body)
 })
