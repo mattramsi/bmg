f2a9817fabe21a719a9618ee6335cf244728db95
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2019-01-03 17:31:31 -0200

    ajustes

--- getMatricula.js
+++ getMatricula.js
@@ -1,5 +1,7 @@
-var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
 
+var saldo = require('./getSaldo');
+
+var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
 var cpf = '44004265053';
 var codigoEntidade = '1581';
 
@@ -39,7 +41,7 @@ const body = request.post(url, opts, (err, response) => {
             var obj = {};
             obj.matricula = multiRef[i].matricula.$t;
             obj.contaInterna = multiRef[i].numeroContaInterna.$t
-            obj.saldo = getSaldo(obj.matricula, obj.contaInterna)
+            obj.saldo = saldo.get(obj.matricula, obj.contaInterna)
     
             array.push(obj);
         }
@@ -48,48 +50,3 @@ const body = request.post(url, opts, (err, response) => {
     console.log(array)
 })
 
-function getSaldo(matricula, contaInterna) {
-
-    
-    var xml = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com"><soapenv:Header/><soapenv:Body><web:buscarLimiteSaque soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'+
-        '<param xsi:type="web:DadosCartaoParameter">'+
-                '<login xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">botz</login>'+
-                '<senha xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">102030mmr@</senha>'+
-                '<codigoEntidade xsi:type="xsd:int">' + codigoEntidade + '</codigoEntidade>'+ 
-                '<cpf xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">'+ matricula + '</cpf>'+
-                '<matricula xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">' + cpf + '</matricula>'+
-                '<numeroContaInterna xsi:type="xsd:long">' + contaInterna + '</numeroContaInterna>'+
-                '<tipoSaque xsi:type="xsd:int">1</tipoSaque>'+
-            '</param>'+
-        '</web:buscarLimiteSaque>'+
-    '</soapenv:Body>'+
-    '</soapenv:Envelope>';
-
-    var arraySaldos = [];
-
-    new Promise(function(resolve, reject) {
-        //GET SALDO
-        const body = request.post(url, opts, (err, response) => {
-        
-            var json = JSON.parse(parser.toJson(response.body));
-
-            var multiRef = json['soapenv:Envelope']['soapenv:Body'].multiRef
-            if(multiRef) {
-                console.log("Body", multiRef)
-                for(var i = 0; i < multiRef.length - 1; i++) {        
-                    if(multiRef[i].valorSaqueMaximo && multiRef[i].valorSaqueMinimo) {
-                        var obj = {};
-                        obj.valorSaqueMinimo = multiRef[i].valorSaqueMinimo.href;
-                        obj.valorSaqueMaximo = multiRef[i].valorSaqueMaximo.href;
-                        arraySaldos.push(obj);
-                    }
-                }
-
-                console.log("saldo", arraySaldos)
-                resolve(arraySaldos);
-            } else{
-                reject(json)
-            }
-        })
-    });
-} 
--- getSaldo.js
+++ getSaldo.js
@@ -1,51 +1,57 @@
 var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
 
-var xml = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com"><soapenv:Header/><soapenv:Body><web:buscarLimiteSaque soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'+
+module.exports = function get(matricula, contaInterna) {
+
+    var xml = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com"><soapenv:Header/><soapenv:Body><web:buscarLimiteSaque soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'+
         '<param xsi:type="web:DadosCartaoParameter">'+
-            '<login xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">botz</login>'+
-            '<senha xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">102030mmr@</senha>'+
-            '<codigoEntidade xsi:type="xsd:int">1581</codigoEntidade>'+
-            '<cpf xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">44004265053</cpf>'+
-            '<matricula xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">1140997480</matricula>'+
-            '<numeroContaInterna xsi:type="xsd:long">8223455</numeroContaInterna>'+
-            '<tipoSaque xsi:type="xsd:int">1</tipoSaque>'+
-        '</param>'+
-      '</web:buscarLimiteSaque>'+
-   '</soapenv:Body>'+
-'</soapenv:Envelope>';
+                '<login xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">botz</login>'+
+                '<senha xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">102030mmr@</senha>'+
+                '<codigoEntidade xsi:type="xsd:int">' + codigoEntidade + '</codigoEntidade>'+ 
+                '<cpf xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">'+ matricula + '</cpf>'+
+                '<matricula xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">' + cpf + '</matricula>'+
+                '<numeroContaInterna xsi:type="xsd:long">' + contaInterna + '</numeroContaInterna>'+
+                '<tipoSaque xsi:type="xsd:int">1</tipoSaque>'+
+            '</param>'+
+        '</web:buscarLimiteSaque>'+
+    '</soapenv:Body>'+
+    '</soapenv:Envelope>';
 
-const request = require('request')
-var parser = require('xml2json');
-
-const opts = {
-    body: xml,
-    headers: {
-        'Content-Type': 'text/xml; charset=utf-8',
-        SOAPAction: 'runTransaction'
-    }
-}
-
-//GET SALDO
-const body = request.post(url, opts, (err, response) => {
-   
-    var json = JSON.parse(parser.toJson(response.body));
-
-    var multiRef = json['soapenv:Envelope']['soapenv:Body'].multiRef
-    // if(multiRef) console.log('response', multiRef)
-
-    var arraySaldos = [];
-    for(var i = 0; i < multiRef.length - 1; i++) {        
-        if(multiRef[i].valorSaqueMaximo && multiRef[i].valorSaqueMinimo) {
-            var obj = {};
-            obj.valorSaqueMinimo = multiRef[i].valorSaqueMinimo.href;
-            obj.valorSaqueMaximo = multiRef[i].valorSaqueMaximo.href;
-            arraySaldos.push(obj);
+    const options = {
+        body: xml,
+        headers: {
+            'Content-Type': 'text/xml; charset=utf-8',
+            SOAPAction: 'runTransaction'
         }
     }
 
-    console.log(arraySaldos)
-})
+    new Promise(function(resolve, reject) {
+        //GET SALDO
+        const body = request.post(url, opts, (err, response) => {
+        
+            var json = JSON.parse(parser.toJson(response.body));
+
+            var multiRef = json['soapenv:Envelope']['soapenv:Body'].multiRef
+
+            var array = [];
+            if(multiRef) {
+                console.log("Body", multiRef)
+                for(var i = 0; i < multiRef.length - 1; i++) {        
+                    if(multiRef[i].valorSaqueMaximo && multiRef[i].valorSaqueMinimo) {
+                        var obj = {};
+                        obj.valorSaqueMinimo = multiRef[i].valorSaqueMinimo.href;
+                        obj.valorSaqueMaximo = multiRef[i].valorSaqueMaximo.href;
+                        arraySaldos.push(obj);
+                    }
+                }
+
+                console.log("arraySaldo", array)
+                resolve(arraySaldos);
+            } else{
+                reject(json)
+            }
+        })
+    });
+}  
 
 
- 
 
