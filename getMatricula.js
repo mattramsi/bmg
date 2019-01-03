6dbbae9cf8f5407ae76e9300ab8865e90ac3457c
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2019-01-03 16:34:16 -0200

    ajustes

---
+++ getMatricula.js
@@ -0,0 +1,35 @@
+var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
+var xml = '<soapenv:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com"><soapenv:Header/><soapenv:Body><web:buscarCartoesDisponiveis >'+
+         '<param>'+
+            '<login>botz</login>'+
+            '<senha>102030mmr@</senha>'+
+            '<codigoEntidade>1581</codigoEntidade>'+
+            '<cpf>44004265053</cpf>'+
+         '</param>'+
+      '</web:buscarCartoesDisponiveis>'+
+   '</soapenv:Body>'+
+'</soapenv:Envelope>';
+
+const request = require('request')
+var parser = require('xml2json');
+
+const opts = {
+    body: xml,
+    headers: {
+        'Content-Type': 'text/xml; charset=utf-8',
+        SOAPAction: 'runTransaction'
+    }
+}
+
+// //GET MATRICULA
+const body = request.post(url, opts, (err, response) => {
+   
+    var json = JSON.parse(parser.toJson(response.body));
+    console.log('response', json)
+
+    var multiRef = json['soapenv:Envelope']['soapenv:Body'].multiRef
+    console.log('response', multiRef)
+
+    
+})
+
