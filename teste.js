52f2ea923481e64fe80e416b923108a8b35625cf
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2019-01-03 14:51:19 -0200

    ajustes

--- teste.js
+++ teste.js
@@ -10,19 +10,19 @@ var xml = '<soapenv:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:
    '</soapenv:Body>'+
 '</soapenv:Envelope>';
 
-// var xml = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com"><soapenv:Header/><soapenv:Body><web:buscarLimiteSaque soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'+
-//         '<param xsi:type="web:DadosCartaoParameter">'+
-//             '<login xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">botz</login>'+
-//             '<senha xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">102030mmr@</senha>'+
-//             '<codigoEntidade xsi:type="xsd:int">1581</codigoEntidade>'+
-//             '<cpf xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">44004265053</cpf>'+
-//             '<matricula xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">1508645423</matricula>'+
-//             '<numeroContaInterna xsi:type="xsd:long">5455945</numeroContaInterna>'+
-//             '<tipoSaque xsi:type="xsd:int">1</tipoSaque>'+
-//         '</param>'+
-//       '</web:buscarLimiteSaque>'+
-//    '</soapenv:Body>'+
-// '</soapenv:Envelope>';
+var xml = '<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com"><soapenv:Header/><soapenv:Body><web:buscarLimiteSaque soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">'+
+        '<param xsi:type="web:DadosCartaoParameter">'+
+            '<login xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">botz</login>'+
+            '<senha xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">102030mmr@</senha>'+
+            '<codigoEntidade xsi:type="xsd:int">1581</codigoEntidade>'+
+            '<cpf xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">44004265053</cpf>'+
+            '<matricula xsi:type="soapenc:string" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/">1140997480</matricula>'+
+            '<numeroContaInterna xsi:type="xsd:long">8223455</numeroContaInterna>'+
+            '<tipoSaque xsi:type="xsd:int">1</tipoSaque>'+
+        '</param>'+
+      '</web:buscarLimiteSaque>'+
+   '</soapenv:Body>'+
+'</soapenv:Envelope>';
 
 const request = require('request')
 var parser = require('xml2json');
