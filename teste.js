6529cdfc5013e469eb93b5836fdbbbe641cbe995
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2019-01-03 11:21:48 -0200

    ajustes

--- teste.js
+++ teste.js
@@ -38,7 +38,7 @@ const opts = {
 const body = request.post(url, opts, (err, response) => {
    
     var json = parser.toJson(response.body);
-     console.log('response', JSON.parse('soapenv:Envelope'))
+     console.log('response', json.parse('soapenv:Envelope'))
  
 })
 
