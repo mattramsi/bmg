1f2a0f2d163e784418ae0d2bfbb7af122ac1b20b
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2019-01-04 15:59:24 -0200

    ajustes

--- relatorio.js
+++ relatorio.js
@@ -26,14 +26,12 @@ var gerarRelatorio = function(array) {
     readCSV().then((response) => {
         var csvData = response; 
 
-        this.array = array;
-
         for(var i = 0; i < csvData.length; i++) {        
             var cpf = csvData[i].cpf
             var codigoEntidade = csvData[i].codigoEntidade
 
             return matricula.get(cpf, codigoEntidade).then((response) => {
-                this.array.push(response)
+                array.push(response)
             })
         }
 
@@ -41,7 +39,7 @@ var gerarRelatorio = function(array) {
     })
 }
 
-gerarRelatorio(array)
+gerarRelatorio()
 
 
 
