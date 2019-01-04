cb5efeb6146e1ba738a5129c8ad3674e530a526c
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2019-01-04 14:18:17 -0200

    relatorio

---
+++ Matricula.js
@@ -0,0 +1,61 @@
+
+var saldo = require('./Saldo');
+const request = require('request')
+var parser = require('xml2json');
+
+var url = 'https://ws1.bmgconsig.com.br/webservices/SaqueComplementar?wsdl';
+
+// // //GET MATRICULA
+module.exports = {
+
+    get: function(cpf, codigoEntidade) {
+
+        var xml = '<soapenv:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:web="http://webservice.econsig.bmg.com"><soapenv:Header/><soapenv:Body><web:buscarCartoesDisponiveis >'+
+                 '<param>'+
+                    '<login>botz</login>'+
+                    '<senha>102030mmr@</senha>'+
+                    '<codigoEntidade>' + codigoEntidade + '</codigoEntidade>'+
+                    '<cpf>' + cpf + '</cpf>'+
+                 '</param>'+
+              '</web:buscarCartoesDisponiveis>'+
+           '</soapenv:Body>'+
+        '</soapenv:Envelope>';
+
+        const opts = { body: xml, headers: { 'Content-Type': 'text/xml; charset=utf-8', SOAPAction: 'runTransaction' }}
+
+        return new Promise((resolve, reject) => {
+            request.post(url, opts, (err, response) => {
+            
+                var json = JSON.parse(parser.toJson(response.body));
+                var multiRef = json['soapenv:Envelope']['soapenv:Body'].multiRef
+
+                if(multiRef) {
+                    var array = [];
+                    for(var i = 0; i < multiRef.length - 1; i++) {  
+                        
+                        if(multiRef[i].matricula && multiRef[i].numeroContaInterna) {
+                            var matricula = multiRef[i].matricula.$t;
+                            var contaInterna = multiRef[i].numeroContaInterna.$t
+
+                            saldo.get(cpf, codigoEntidade, matricula, contaInterna).then( (response) => {
+                                
+                                var obj = {};
+                                obj.matricula = matricula;
+                                obj.contaInterna = contaInterna
+                                obj.saldo = response
+                                array.push(obj);
+                                console.log(JSON.stringify(array))
+                            })   
+                        }
+                    }
+
+                    resolve(array)
+                } else{
+                    reject("Não foi possivel gerar o relatório");
+                }
+                
+            }); 
+        })
+    }
+}
+
---
+++ relatorio.js
@@ -0,0 +1,21 @@
+var XLSX = require('xlsx')
+var matricula = require('./Matricula');
+
+var workbook = XLSX.readFile('ListaDeCpfs.xlsx');
+var sheet_name_list = workbook.SheetNames;
+var xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+console.log(xlData.length);
+
+function gerarRelatorio() {
+
+    for(var i = 0; i < xlData.length - 1; i++) {        
+       var cpf = xlData[i].cpf
+       var codigoEntidade = xlData[i].codigoEntidade
+
+       matricula.get(cpf, codigoEntidade).then((response) => {
+            console.log(response)
+       })
+    }
+
+}
+
