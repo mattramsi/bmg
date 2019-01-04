84961bfe9f15f616c66202a64c95b9e4e340874a
Matheus Ramos <matheusramos@Air-de-Matheus.localdomain>
2019-01-04 15:20:15 -0200

    ajustes

--- relatorio.js
+++ relatorio.js
@@ -2,32 +2,54 @@
 var matricula = require('./Matricula');
 var fs = require('fs'); 
 var parse = require('csv-parser');
+var json2csv = require('json2csv');
 
-var csvData=[];
-fs.createReadStream('ListaDeCpfs.csv')
-    .pipe(parse({delimiter: ':'}))
-    .on('data', function(csvrow) {
-        csvData.push(csvrow);        
-    })
-    .on('end',function() {
-      //do something wiht csvData
-      console.log(csvData);
-      gerarRelatorio()
+var readCSV = function() {
+
+    var csvData=[];
+    return new Promise((resolve, reject) => {
+
+        fs.createReadStream('ListaDeCpfs.csv')
+        .pipe(parse({delimiter: ':'}))
+        .on('data', function(csvrow) {
+            csvData.push(csvrow);        
+        })
+        .on('end',function() {
+            //do something wiht csvData
+            resolve(csvData);
+        });
     });
+}
+
 
 var gerarRelatorio = function() {
-    console.log(csvData.length)
-    for(var i = 0; i < csvData.length; i++) {        
-        var cpf = csvData[i].cpf
-        var codigoEntidade = csvData[i].codigoEntidade
 
-        matricula.get(cpf, codigoEntidade).then((response) => {
+    readCSV().then((response) => {
+        var csvData = response; 
 
-                console.log("oi" + i , JSON.stringify(response))
-                
-        })
-    }
+        var array = [];
+        for(var i = 0; i < csvData.length; i++) {        
+            var cpf = csvData[i].cpf
+            var codigoEntidade = csvData[i].codigoEntidade
+
+            matricula.get(cpf, codigoEntidade).then((response) => {
+                array.push(response)
+            })
+        }
+
+        console.log(JSON.stringify(array))
+        json2csv({data: array, fields: ['cpf', 'codigoEntidade', 'valorSaqueMinimo', 'valorSaqueMaximo']}, function(err, csv) {
+            if (err) console.log(err);
+            fs.writeFile('cars.csv', csv, function(err) {
+                if (err) throw err;
+                console.log('cars file saved');
+            });
+        });
+    })
+  
 }
+
+gerarRelatorio()
 
 
 
