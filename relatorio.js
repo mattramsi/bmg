d40b8a1db28a37f6c512d04367a7a8f99f9ba57c
Matheus Ramos <matheusramos@Air-de-Matheus.lan>
2019-01-06 19:42:01 -0200

    ajustes

--- relatorio.js
+++ relatorio.js
@@ -49,8 +49,6 @@ var gerarRelatorio = function() {
                 })
                 
                 if(i == (10-1) ){
-                    console.log(JSON.stringify(array))
-
                     var xls = json2xls(array);
                     fs.writeFileSync('Relatório.xlsx', xls, 'binary');
                 } 
